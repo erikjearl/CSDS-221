54e7a32bdf3421def65d0701dacb38b02bac0797
erikjearl <erikjearl@gmail.com>
2022-11-17 17:55:13 -0500

    form validation
    validate
    and date reforming

--- pr02/pr02/src/components/Modal.vue
+++ pr02/pr02/src/components/Modal.vue
@@ -13,51 +13,55 @@
             </div>
             
           </v-card-title>
-
             <!--panel body-->
-            <v-layout v-if="isAddTask" justify-center class="elements mx-5 mb-0 mt-7" >
-              <v-text-field label="Title" outlined v-model="title"/>
-            </v-layout>
+            <v-form ref="form" lazy-validation>
+              <v-layout v-if="isAddTask" justify-center class="elements mx-5 mb-0 mt-7" >
+                <v-text-field label="Title" outlined v-model="title" 
+                  :rules="[v => !!v || 'Title is required']" required />
+              </v-layout>
 
-            <v-layout justify-center class="element mx-5 my-2">
-              <v-text-field label="Description" outlined v-model="description"/>
-            </v-layout>
+              <v-layout justify-center class="element mx-5 my-2">
+                <v-text-field label="Description" outlined v-model="description"
+                  :rules="[v => !!v || 'Description is required']" required />
+              </v-layout>
 
-            <v-layout justify-center class="elements mx-5 my-0">
-              <v-menu ref="menu" :close-on-content-click="false" :return-value.sync="deadline" transition="scale-transition" offset-y min-width="auto">
-                <template v-slot:activator="{ on, attrs }">
-                  <v-text-field v-model="deadline" label="Deadline" append-icon="mdi-calendar" v-bind="attrs" v-on="on" readonly outlined/>
-                </template>
-                <v-date-picker v-model="deadline" no-title scrollable>
-                  <v-spacer></v-spacer>
-                  <v-btn text color="primary" @click="menu = false"> Cancel </v-btn>
-                  <v-btn text color="primary" @click="$refs.menu.save(deadline)">OK</v-btn>
-                </v-date-picker>
-              </v-menu>
-            </v-layout>
 
-              <v-radio-group label="Priority" class="elements mx-4 my-0" v-model="priority">
-                <v-layout align-start row d-flex justify-space-between>
-                  <v-radio label="Low" value="low"/>  &nbsp;
-                  <v-radio label="Med" value="med" />  &nbsp;
-                  <v-radio label="High" value="high" />
-                </v-layout>
-              </v-radio-group>
+              <v-layout justify-center class="elements mx-5 my-0">
+                <v-menu ref="menu" :close-on-content-click="false" :return-value.sync="deadline" transition="scale-transition" offset-y min-width="auto">
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field v-model="deadline" label="Deadline" append-icon="mdi-calendar" v-bind="attrs" v-on="on" readonly outlined
+                      :rules="[v => !!v || 'Deadline is required']" required />
+                  </template>
+                  <v-date-picker v-model="date" no-title scrollable>
+                    <v-spacer></v-spacer>
+                    <v-btn text color="primary" @click="menu = false"> Cancel </v-btn>
+                    <v-btn text color="primary" @click="$refs.menu.save(date)">OK</v-btn>
+                  </v-date-picker>
+                </v-menu>
+              </v-layout>
 
-            <v-layout justify-end class="elements mx-2 mt-0 mb-2">
-              <div v-if="isAddTask" key="add" >
-                <v-btn color="primary" elevation="2" @click="addTask" class="mr-2">  
-                    <i class="fa-solid fa-circle-plus"></i> ADD 
-                </v-btn>
+                <v-radio-group label="Priority" class="elements mx-4 my-0" v-model="priority">
+                  <v-layout align-start row d-flex justify-space-between>
+                    <v-radio label="Low" value="low"/>  &nbsp;
+                    <v-radio label="Med" value="med" />  &nbsp;
+                    <v-radio label="High" value="high" />
+                  </v-layout>
+                </v-radio-group>
+
+              <v-layout justify-end class="elements mx-2 mt-0 mb-2">
+                <div v-if="isAddTask" key="add" >
+                  <v-btn color="primary" elevation="2" @click="addTask" class="mr-2">  
+                      <i class="fa-solid fa-circle-plus"></i> ADD 
+                  </v-btn>
+                </div>
+                <div v-else key="update">
+                  <v-btn color="primary" elevation="2" @click="updateTask" class="mr-2"> 
+                      <i class="fa-solid fa-pen-to-square"></i> EDIT
+                  </v-btn>
               </div>
-              <div v-else key="update">
-                <v-btn color="primary" elevation="2" @click="updateTask" class="mr-2"> 
-                    <i class="fa-solid fa-pen-to-square"></i> EDIT
-                </v-btn>
-            </div>
-            <v-btn color="error" elevation="2" @click="close"> <span class="fa-solid fa-ban"/> CANCEL</v-btn>
-            </v-layout>
-
+              <v-btn color="error" elevation="2" @click="close"> <span class="fa-solid fa-ban"/> CANCEL</v-btn>
+              </v-layout>
+            </v-form>
           </v-card>
       </div>
     </div>
@@ -78,21 +82,31 @@
     },
     watch:{
       isModalVisible(newVal){
+        this.$refs.form.resetValidation();
         if(newVal && !this.isAddTask){
           this.getParentData();
         }
-      }
+      },
+      deadline() {
+        if(this.deadline.includes("-")){
+          this.deadline = this.formatDate(this.deadline)
+        }
+      },
     },
     methods: {
       addTask(){
-        this.$emit('addTask', this.title, this.description, this.deadline, this.priority);
-        this.clear();
-        this.close();
+        if(this.$refs.form.validate()){
+          this.$emit('addTask', this.title, this.description, this.deadline, this.priority);
+          this.clear();
+          this.close();
+        }
       },
       updateTask(){
-        this.$emit('updateTask', this.description, this.deadline, this.priority);
-        this.clear();
-        this.close();
+        if(this.$refs.form.validate()){
+          this.$emit('updateTask', this.description, this.deadline, this.priority);
+          this.clear();
+          this.close();
+        }
       },
       close() {
         this.clear();
@@ -103,12 +117,18 @@
         this.description='';
         this.deadline='';
         this.priority='low';
+        this.$refs.form.resetValidation();
       },
       getParentData(){
         this.description=this.existing_description;
         this.deadline=this.existing_deadline;
         this.priority=this.existing_priority;
-      }
+      },
+      formatDate(date){
+        if (!date) return null;
+        const [year, month, day] = date.split('-');
+        return `${month}/${day}/${year}`;
+      },
     },
     data(){
       return{
@@ -116,6 +136,7 @@
         description: '',
         deadline: '',
         priority: 'low',
+        date:'',
       }
     }
   };
